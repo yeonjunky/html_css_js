6f4b81456329ee74ba391a1f5fb87ce5e7cb05cb
yeonjunky <yeonjunky@gmail.com>
2020-03-15 03:08:39 +0900

    .

--- html/nicolas/index.js
+++
@@ -1,5 +0,0 @@
-// alert('Hello world! Im javascript!');
-console.log('Hello world!');
-a = 221;
-b = a - 5;
-console.log(b);
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,20 @@
 const asfd = 123;
 var skf = 123;
 
-console.log("hello world!");
+// console.log("hello world!dsaf");
+
+const info = {
+    name : "yeonjun",
+    age : 19,
+    gender : "male",
+
+}
+
+function Hello(name){
+    console.log("Hello " + name);
+}
+
+console.log(info);
+
+Hello('Jun');
+
