5bc55b7d06138684103eaab85d918a913cd3182e
yeonjunky <yeonjunky@gmail.com>
2020-03-15 16:50:53 +0900

    .

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+const asfd = 123;
+var skf = 123;
+
+// console.log("hello world!dsaf");
+
+// const info = {
+//     name : "yeonjun",
+//     age : 19,
+//     gender : "male",
+
+// }
+
+// function Hello(name){
+//     console.log("Hello " + name);
+// }
+
+// console.log(info);
+
+// Hello('Jun');
+
+// console.log(document);
+// console.error("error!");
+const head = document.getElementsByTagName("h1");
+console.log(head);
+const title = document.getElementById("title");
+title.innerHTML = 'hi from js';
+console.log(title);
+console.dir(title);
